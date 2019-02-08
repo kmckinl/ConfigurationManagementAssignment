5012c7c13014c0ebdc3970f2f2d91c09cdb0b8a8
Kyle McKinley <kmckinley@protonmail.com>
2019-02-07 19:45:11 -0500

    Implented Ninety_Nine_Bottles_of_Beer() method which prints to the console the 99 bottles song

--- BeerSong.py
+++ BeerSong.py
@@ -2,6 +2,15 @@ def Main():
     Ninety_Nine_Bottles_of_Beer()
     
 def Ninety_Nine_Bottles_of_Beer():
+    for x in range(0, 99):
+        y = 99 - x
+        z = y - 1
+        if (y > 1):
+            print(str(y) + " bottles of beer on the wall, " + str(y)  +" bottles of beer")
+            print("take one down, pass it around, " + str(z)  + " bottles of beer on the wall")
+        else:
+            print(str(y) + " bottles of beer on the wall, " + str(y)  +" bottles of beer")
+            print("take one down, pass it around, no more bottles of beer on the wall")
             
 Main()  
     
